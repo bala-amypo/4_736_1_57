8e67325d6f1dafcda721e7105e17b2191305307b
bala-amypo <bala@amypo.in>
2025-12-18 11:22:28 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,9 +8,16 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		switch (jkgdjgjgnbdfbnc c,m,b,cvmbcvmbmbn,cvb     c , ,v  nb             bnnbbnbnnb                                                       dk fn) {
+			case value:
+				
+				break;
+		
+			default:
+				break;k
+		}ystem.err.println('hi);
 	}
 
-}
-// mvn clean package -Pfull spring-boot:repackage
+}// mvn clean package -Pfull spring-boot:repackage
 // stress --vm 1 --vm-bytes 512M --timeout 10s
 
